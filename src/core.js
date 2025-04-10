450cdcf917cd90b48d7bd90c0cfa911dc19044be
Qi, Xiaoqiang <qixiaoqiang@giantdc.com>
2025-04-10 10:25:55 +0800

    1.调用configure方法时，如果config设置了radius，则调整先前所有数据的radius 2.增加禁用设置container的position的配置

--- src/core.js
+++ src/core.js
@@ -1,142 +1,151 @@
 // Heatmap Constructor
 var Heatmap = (function HeatmapClosure() {
-
   var Coordinator = (function CoordinatorClosure() {
-
     function Coordinator() {
-      this.cStore = {};
-    };
+      this.cStore = {}
+    }
 
     Coordinator.prototype = {
-      on: function(evtName, callback, scope) {
-        var cStore = this.cStore;
+      on: function (evtName, callback, scope) {
+        var cStore = this.cStore
 
         if (!cStore[evtName]) {
-          cStore[evtName] = [];
+          cStore[evtName] = []
         }
-        cStore[evtName].push((function(data) {
-            return callback.call(scope, data);
-        }));
+        cStore[evtName].push(function (data) {
+          return callback.call(scope, data)
+        })
       },
-      emit: function(evtName, data) {
-        var cStore = this.cStore;
+      emit: function (evtName, data) {
+        var cStore = this.cStore
         if (cStore[evtName]) {
-          var len = cStore[evtName].length;
-          for (var i=0; i<len; i++) {
-            var callback = cStore[evtName][i];
-            callback(data);
+          var len = cStore[evtName].length
+          for (var i = 0; i < len; i++) {
+            var callback = cStore[evtName][i]
+            callback(data)
           }
         }
-      }
-    };
+      },
+    }
 
-    return Coordinator;
-  })();
+    return Coordinator
+  })()
 
+  var _connect = function (scope) {
+    var renderer = scope._renderer
+    var coordinator = scope._coordinator
+    var store = scope._store
 
-  var _connect = function(scope) {
-    var renderer = scope._renderer;
-    var coordinator = scope._coordinator;
-    var store = scope._store;
-
-    coordinator.on('renderpartial', renderer.renderPartial, renderer);
-    coordinator.on('renderall', renderer.renderAll, renderer);
-    coordinator.on('extremachange', function(data) {
+    coordinator.on('renderpartial', renderer.renderPartial, renderer)
+    coordinator.on('renderall', renderer.renderAll, renderer)
+    coordinator.on('extremachange', function (data) {
       scope._config.onExtremaChange &&
-      scope._config.onExtremaChange({
-        min: data.min,
-        max: data.max,
-        gradient: scope._config['gradient'] || scope._config['defaultGradient']
-      });
-    });
-    store.setCoordinator(coordinator);
-  };
-
+        scope._config.onExtremaChange({
+          min: data.min,
+          max: data.max,
+          gradient:
+            scope._config['gradient'] || scope._config['defaultGradient'],
+        })
+    })
+    store.setCoordinator(coordinator)
+  }
 
   function Heatmap() {
-    var config = this._config = Util.merge(HeatmapConfig, arguments[0] || {});
-    this._coordinator = new Coordinator();
+    var config = (this._config = Util.merge(HeatmapConfig, arguments[0] || {}))
+    this._coordinator = new Coordinator()
     if (config['plugin']) {
-      var pluginToLoad = config['plugin'];
+      var pluginToLoad = config['plugin']
       if (!HeatmapConfig.plugins[pluginToLoad]) {
-        throw new Error('Plugin \''+ pluginToLoad + '\' not found. Maybe it was not registered.');
+        throw new Error(
+          "Plugin '" +
+            pluginToLoad +
+            "' not found. Maybe it was not registered."
+        )
       } else {
-        var plugin = HeatmapConfig.plugins[pluginToLoad];
+        var plugin = HeatmapConfig.plugins[pluginToLoad]
         // set plugin renderer and store
-        this._renderer = new plugin.renderer(config);
-        this._store = new plugin.store(config);
+        this._renderer = new plugin.renderer(config)
+        this._store = new plugin.store(config)
       }
     } else {
-      this._renderer = new Renderer(config);
-      this._store = new Store(config);
+      this._renderer = new Renderer(config)
+      this._store = new Store(config)
     }
-    _connect(this);
-  };
+    _connect(this)
+  }
 
   // @TODO:
   // add API documentation
   Heatmap.prototype = {
-    addData: function() {
-      this._store.addData.apply(this._store, arguments);
-      return this;
+    addData: function () {
+      this._store.addData.apply(this._store, arguments)
+      return this
     },
-    removeData: function() {
-      this._store.removeData && this._store.removeData.apply(this._store, arguments);
-      return this;
+    removeData: function () {
+      this._store.removeData &&
+        this._store.removeData.apply(this._store, arguments)
+      return this
     },
-    setData: function() {
-      this._store.setData.apply(this._store, arguments);
-      return this;
+    setData: function () {
+      this._store.setData.apply(this._store, arguments)
+      return this
     },
-    setDataMax: function() {
-      this._store.setDataMax.apply(this._store, arguments);
-      return this;
+    setDataMax: function () {
+      this._store.setDataMax.apply(this._store, arguments)
+      return this
     },
-    setDataMin: function() {
-      this._store.setDataMin.apply(this._store, arguments);
-      return this;
+    setDataMin: function () {
+      this._store.setDataMin.apply(this._store, arguments)
+      return this
     },
-    configure: function(config) {
-      this._config = Util.merge(this._config, config);
-      this._renderer.updateConfig(this._config);
-      this._coordinator.emit('renderall', this._store._getInternalData());
-      return this;
-    },
-    repaint: function() {
-      this._coordinator.emit('renderall', this._store._getInternalData());
-      return this;
-    },
-    getData: function() {
-      return this._store.getData();
-    },
-    getDataURL: function() {
-      return this._renderer.getDataURL();
-    },
-    getValueAt: function(point) {
-
-      if (this._store.getValueAt) {
-        return this._store.getValueAt(point);
-      } else  if (this._renderer.getValueAt) {
-        return this._renderer.getValueAt(point);
+    configure: function (config) {
+      this._config = Util.merge(this._config, config)
+      this._renderer.updateConfig(this._config)
+      var radius = config['radius']
+      if (radius === undefined || radius === null) {
+        this._coordinator.emit('renderall', this._store._getInternalData())
       } else {
-        return null;
+        this._store.updateCfgRadius(radius)
+        var internalData = this._store._getInternalData()
+        var radi = internalData.radi?.map((item) => item?.map((item) => radius))
+        internalData.radi = radi
+        this._coordinator.emit('renderall', internalData)
       }
-    }
-  };
 
-  return Heatmap;
+      return this
+    },
+    repaint: function () {
+      this._coordinator.emit('renderall', this._store._getInternalData())
+      return this
+    },
+    getData: function () {
+      return this._store.getData()
+    },
+    getDataURL: function () {
+      return this._renderer.getDataURL()
+    },
+    getValueAt: function (point) {
+      if (this._store.getValueAt) {
+        return this._store.getValueAt(point)
+      } else if (this._renderer.getValueAt) {
+        return this._renderer.getValueAt(point)
+      } else {
+        return null
+      }
+    },
+  }
 
-})();
-
+  return Heatmap
+})()
 
 // core
 var heatmapFactory = {
-  create: function(config) {
-    return new Heatmap(config);
+  create: function (config) {
+    return new Heatmap(config)
   },
-  register: function(pluginKey, plugin) {
-    HeatmapConfig.plugins[pluginKey] = plugin;
-  }
-};
+  register: function (pluginKey, plugin) {
+    HeatmapConfig.plugins[pluginKey] = plugin
+  },
+}
 
-return heatmapFactory;
+return heatmapFactory
--- src/data.js
+++ src/data.js
@@ -1,173 +1,169 @@
 var Store = (function StoreClosure() {
-
   var Store = function Store(config) {
-    this._coordinator = {};
-    this._data = [];
-    this._radi = [];
-    this._min = 10;
-    this._max = 1;
-    this._xField = config['xField'] || config.defaultXField;
-    this._yField = config['yField'] || config.defaultYField;
-    this._valueField = config['valueField'] || config.defaultValueField;
+    this._coordinator = {}
+    this._data = []
+    this._radi = []
+    this._min = 10
+    this._max = 1
+    this._xField = config['xField'] || config.defaultXField
+    this._yField = config['yField'] || config.defaultYField
+    this._valueField = config['valueField'] || config.defaultValueField
 
-    if (config["radius"]) {
-      this._cfgRadius = config["radius"];
+    if (config['radius']) {
+      this._cfgRadius = config['radius']
     }
-  };
+  }
 
-  var defaultRadius = HeatmapConfig.defaultRadius;
+  var defaultRadius = HeatmapConfig.defaultRadius
 
   Store.prototype = {
     // when forceRender = false -> called from setData, omits renderall event
-    _organiseData: function(dataPoint, forceRender) {
-        var x = dataPoint[this._xField];
-        var y = dataPoint[this._yField];
-        var radi = this._radi;
-        var store = this._data;
-        var max = this._max;
-        var min = this._min;
-        var value = dataPoint[this._valueField] || 1;
-        var radius = dataPoint.radius || this._cfgRadius || defaultRadius;
+    _organiseData: function (dataPoint, forceRender) {
+      var x = dataPoint[this._xField]
+      var y = dataPoint[this._yField]
+      var radi = this._radi
+      var store = this._data
+      var max = this._max
+      var min = this._min
+      var value = dataPoint[this._valueField] || 1
+      var radius = dataPoint.radius || this._cfgRadius || defaultRadius
 
-        if (!store[x]) {
-          store[x] = [];
-          radi[x] = [];
-        }
+      if (!store[x]) {
+        store[x] = []
+        radi[x] = []
+      }
 
-        if (!store[x][y]) {
-          store[x][y] = value;
-          radi[x][y] = radius;
+      if (!store[x][y]) {
+        store[x][y] = value
+        radi[x][y] = radius
+      } else {
+        store[x][y] += value
+      }
+      var storedVal = store[x][y]
+
+      if (storedVal > max) {
+        if (!forceRender) {
+          this._max = storedVal
         } else {
-          store[x][y] += value;
+          this.setDataMax(storedVal)
         }
-        var storedVal = store[x][y];
-
-        if (storedVal > max) {
-          if (!forceRender) {
-            this._max = storedVal;
-          } else {
-            this.setDataMax(storedVal);
-          }
-          return false;
-        } else if (storedVal < min) {
-          if (!forceRender) {
-            this._min = storedVal;
-          } else {
-            this.setDataMin(storedVal);
-          }
-          return false;
+        return false
+      } else if (storedVal < min) {
+        if (!forceRender) {
+          this._min = storedVal
         } else {
-          return { 
-            x: x, 
-            y: y,
-            value: value, 
-            radius: radius,
-            min: min,
-            max: max 
-          };
+          this.setDataMin(storedVal)
         }
+        return false
+      } else {
+        return {
+          x: x,
+          y: y,
+          value: value,
+          radius: radius,
+          min: min,
+          max: max,
+        }
+      }
     },
-    _unOrganizeData: function() {
-      var unorganizedData = [];
-      var data = this._data;
-      var radi = this._radi;
+    _unOrganizeData: function () {
+      var unorganizedData = []
+      var data = this._data
+      var radi = this._radi
 
       for (var x in data) {
         for (var y in data[x]) {
-
           unorganizedData.push({
             x: x,
             y: y,
             radius: radi[x][y],
-            value: data[x][y]
-          });
-
+            value: data[x][y],
+          })
         }
       }
       return {
         min: this._min,
         max: this._max,
-        data: unorganizedData
-      };
+        data: unorganizedData,
+      }
     },
-    _onExtremaChange: function() {
+    _onExtremaChange: function () {
       this._coordinator.emit('extremachange', {
         min: this._min,
-        max: this._max
-      });
+        max: this._max,
+      })
     },
-    addData: function() {
+    addData: function () {
       if (arguments[0].length > 0) {
-        var dataArr = arguments[0];
-        var dataLen = dataArr.length;
+        var dataArr = arguments[0]
+        var dataLen = dataArr.length
         while (dataLen--) {
-          this.addData.call(this, dataArr[dataLen]);
+          this.addData.call(this, dataArr[dataLen])
         }
       } else {
-        // add to store  
-        var organisedEntry = this._organiseData(arguments[0], true);
+        // add to store
+        var organisedEntry = this._organiseData(arguments[0], true)
         if (organisedEntry) {
           // if it's the first datapoint initialize the extremas with it
           if (this._data.length === 0) {
-            this._min = this._max = organisedEntry.value;
+            this._min = this._max = organisedEntry.value
           }
           this._coordinator.emit('renderpartial', {
             min: this._min,
             max: this._max,
-            data: [organisedEntry]
-          });
+            data: [organisedEntry],
+          })
         }
       }
-      return this;
+      return this
     },
-    setData: function(data) {
-      var dataPoints = data.data;
-      var pointsLen = dataPoints.length;
-
+    setData: function (data) {
+      var dataPoints = data.data
+      var pointsLen = dataPoints.length
 
       // reset data arrays
-      this._data = [];
-      this._radi = [];
+      this._data = []
+      this._radi = []
 
-      for(var i = 0; i < pointsLen; i++) {
-        this._organiseData(dataPoints[i], false);
+      for (var i = 0; i < pointsLen; i++) {
+        this._organiseData(dataPoints[i], false)
       }
-      this._max = data.max;
-      this._min = data.min || 0;
-      
-      this._onExtremaChange();
-      this._coordinator.emit('renderall', this._getInternalData());
-      return this;
+      this._max = data.max
+      this._min = data.min || 0
+
+      this._onExtremaChange()
+      this._coordinator.emit('renderall', this._getInternalData())
+      return this
     },
-    removeData: function() {
+    removeData: function () {
       // TODO: implement
     },
-    setDataMax: function(max) {
-      this._max = max;
-      this._onExtremaChange();
-      this._coordinator.emit('renderall', this._getInternalData());
-      return this;
+    setDataMax: function (max) {
+      this._max = max
+      this._onExtremaChange()
+      this._coordinator.emit('renderall', this._getInternalData())
+      return this
     },
-    setDataMin: function(min) {
-      this._min = min;
-      this._onExtremaChange();
-      this._coordinator.emit('renderall', this._getInternalData());
-      return this;
+    setDataMin: function (min) {
+      this._min = min
+      this._onExtremaChange()
+      this._coordinator.emit('renderall', this._getInternalData())
+      return this
     },
-    setCoordinator: function(coordinator) {
-      this._coordinator = coordinator;
+    setCoordinator: function (coordinator) {
+      this._coordinator = coordinator
     },
-    _getInternalData: function() {
-      return { 
+    _getInternalData: function () {
+      return {
         max: this._max,
-        min: this._min, 
+        min: this._min,
         data: this._data,
-        radi: this._radi 
-      };
+        radi: this._radi,
+      }
     },
-    getData: function() {
-      return this._unOrganizeData();
-    }/*,
+    getData: function () {
+      return this._unOrganizeData()
+    } /*,
 
       TODO: rethink.
 
@@ -205,9 +201,11 @@ var Store = (function StoreClosure() {
         }
       }
       return false;
-    }*/
-  };
+    }*/,
+    updateCfgRadius(radius) {
+      this._cfgRadius = radius
+    },
+  }
 
-
-  return Store;
-})();
+  return Store
+})()
